7fef114fe76be7f6e599b75b93f1987e0951151a
Vincent Lafeychine <vincent.lafeychine@gmail.com>
2021-11-23 21:43:02 +0100

    Change internal structures from Hashtbl to Map

--- typing.ml
+++ typing.ml
@@ -11,8 +11,13 @@ let error loc e = raise (Error (loc, e))
 
 
 (* NOTE Créations des contextes *)
-let contexte_structures: (string, structure) Hashtbl.t = Hashtbl.create 0
-let contexte_functions: (string, function_) Hashtbl.t = Hashtbl.create 0
+module Contexte = struct
+  module M = Map.Make(String)
+
+  let add = M.add
+  let create = M.empty
+  let find = M.find_opt
+end
 
 
 let found_main = ref false
@@ -35,16 +40,16 @@ let rec get_tast_type_name = function
 
 
 (* NOTE Récupération, si existant, du type correspondant à la chaîne de caractère *)
-let rec type_opt = function
+let rec type_opt structures = function
   | PTident { id = "int" } -> Some Tint
   | PTident { id = "bool" } -> Some Tbool
   | PTident { id = "string" } -> Some Tstring
 
-  | PTident { id } -> ( match Hashtbl.find_opt contexte_structures id with
+  | PTident { id } -> ( match Contexte.find id structures with
       | Some struct_type -> Some (Tstruct struct_type)
       | None             -> None )
 
-  | PTptr ty -> ( match type_opt ty with
+  | PTptr ty -> ( match type_opt structures  ty with
       | Some sub_type -> Some (Tptr sub_type)
       | None         -> None )
 
@@ -76,33 +81,6 @@ let new_var =
   fun name loc typ ?(used=false) ->
     incr id;
     { v_name = name; v_id = !id; v_loc = loc; v_typ = typ; v_used = used; v_addr = false }
-
-
-(* NOTE Environnement des variables de fonction *)
-module Env = struct
-  module M = Map.Make(String)
-  type t = var M.t
-
-  let empty = M.empty
-
-  let find = M.find
-
-  let add env v = M.add v.v_name v env
-
-  let all_vars = ref []
-
-  let check_unused () =
-    let check v =
-      if v.v_name <> "_" && (* TODO used *) true then error v.v_loc "unused variable" in
-    List.iter check !all_vars
-
-  let var x loc ?used ty env =
-    let v = new_var x loc ty ?used in
-    all_vars := v :: !all_vars;
-    add env v, v
-
-  (* TODO type () et vecteur de types *)
-end
 
 
 (* NOTE Vérifie si un bloc contient un return, et plus rien après *)
@@ -149,8 +127,7 @@ and expr_desc env loc = function
       | _ -> (* TODO *) error loc ("no such type " ^ id) in
     new_expr (TEnew ty) (Tptr ty), false
 
-  | PEcall ({ id = "new" }, _) ->
-    error loc "new expects a type"
+  | PEcall ({ id = "new" }, _) -> error loc "new expects a type"
 
   | PEcall (id, el) ->
     (* TODO *) assert false
@@ -166,12 +143,11 @@ and expr_desc env loc = function
       | { expr_desc = TEskip } -> new_stmt (TEif (e1, e2, e3)), rt_if
       | _ -> new_stmt (TEif (e1, e2, e3)), rt_if && rt_else );
 
-  | PEnil ->
-    (* TODO *) assert false
+  | PEnil -> new_stmt TEnil, true
 
-  | PEident { id } ->
-    (* TODO *) (try let v = Env.find id env in new_expr (TEident v) v.v_typ, false
-                with Not_found -> error loc ("unbound variable " ^ id))
+  | PEident { id } -> assert false
+  (* (\* TODO *\) (try let v = Env.find id env in new_expr (TEident v) v.v_typ, false *)
+  (*             with Not_found -> error loc ("unbound variable " ^ id)) *)
 
   | PEdot (e, id) ->
     (* TODO *) assert false
@@ -193,20 +169,21 @@ and expr_desc env loc = function
 
 
 (* 1. declare structures *)
-let phase1 = function
+let phase1 structures = function
   | PDstruct { ps_name = { id; loc } } ->
     (* NOTE Vérification de l'unicité des noms de structures *)
-    if Hashtbl.find_opt contexte_structures id <> None then
+    if Contexte.find id structures <> None then
       error loc (Printf.sprintf "structure %s redeclared" id);
 
     (* NOTE Ajout des structures dans le contexte de typage sans les champs *)
-    Hashtbl.add contexte_structures id { s_name = id; s_fields = Hashtbl.create 5 }
+    Contexte.add id { s_name = id; s_fields = Hashtbl.create 5 } structures
 
-  | _ -> ()
+
+  | _ -> structures
 
 
 (* 2. declare functions and type fields *)
-let phase2 = function
+let phase2 (structures, functions) = function
   | PDfunction { pf_name = { id; loc }; pf_params = pl; pf_typ = tyl } ->
     (* NOTE Vérification de la fonction main sans paramètres et sans type de retour *)
     if id = "main" then (
@@ -215,7 +192,7 @@ let phase2 = function
       found_main := true );
 
     (* NOTE Vérification de l'unicité des noms de fonctions *)
-    if Hashtbl.find_opt contexte_functions id <> None then
+    if Contexte.find id functions <> None then
       error loc (Printf.sprintf "function %s redeclared" id);
 
     (* NOTE Vérification de l'unicité des noms des paramètres *)
@@ -228,7 +205,7 @@ let phase2 = function
     (* NOTE Vérification de la bonne formation de chacun des paramètres *)
     let fn_params =
       let param_to_var ({ id = id_param; loc = loc_param }, type_param) =
-        match type_opt type_param with
+        match type_opt structures type_param with
         | Some typ -> new_var id_param loc_param typ ~used:false
         | None -> error loc_param (Printf.sprintf "undefined type %s of parameter %s in function %s" (get_ast_type_name type_param) id_param id)
       in List.map param_to_var pl in
@@ -236,15 +213,15 @@ let phase2 = function
     (* NOTE Vérification de la bonne formation de chacun des valeurs de retours *)
     let fn_typ =
       let get_type_return_value type_return_value =
-        match type_opt type_return_value with
+        match type_opt structures type_return_value with
         | Some value -> value
         | None -> error loc (Printf.sprintf "undefined type %s of one of return values in function %s" (get_ast_type_name type_return_value) id)
       in list_type (List.map get_type_return_value tyl) in
 
-    Hashtbl.add contexte_functions id { fn_name = id; fn_params; fn_typ }
+    (structures, Contexte.add id { fn_name = id; fn_params; fn_typ } functions)
 
   | PDstruct { ps_name = { id }; ps_fields = fl } ->
-    let { s_fields } as structure = Hashtbl.find contexte_structures id in
+    let { s_fields } as structure = Option.get (Contexte.find id structures) in
 
     let process_struct_field ({ id = id_field; loc = loc_field }, type_field) =
       (* NOTE Vérification de l'unicité des champs de la structure *)
@@ -252,13 +229,14 @@ let phase2 = function
         error loc_field (Printf.sprintf "duplicate field %s in structure %s" id_field id);
 
       (* NOTE Vérification de la bonne formation du type *)
-      let f_typ = match type_opt type_field with
+      let f_typ = match type_opt structures type_field with
         | Some f_typ -> f_typ;
         | None -> error loc_field (Printf.sprintf "undefined type %s of field %s in structure %s" (get_ast_type_name type_field) id_field id) in
 
-      Hashtbl.add s_fields id_field { f_name = id_field; f_typ; f_ofs = 0 }
+      Hashtbl.add s_fields id_field { f_name = id_field; f_typ; f_ofs = 0 } in
 
-    in List.iter process_struct_field fl
+    List.iter process_struct_field fl;
+    (structures, functions)
 
 
 (* 3. type check function bodies *)
@@ -270,11 +248,11 @@ let rec sizeof = function
   | _ -> (* TODO *) assert false
 
 
-let decl = function
+let decl structures functions = function
   | PDfunction { pf_name = { id; loc }; pf_body = e; pf_typ = tyl } ->
-    let { fn_name; fn_typ } as fn = Hashtbl.find contexte_functions id in
+    let { fn_name; fn_typ } as fn = Option.get (Contexte.find id functions) in
 
-    let (e, rt) = expr Env.empty e in
+    let (e, rt) = expr Contexte.create e in
 
     (* NOTE Vérification de chacun des return *)
     ( let rec iter_return_stmt f { expr_desc } =
@@ -302,7 +280,7 @@ let decl = function
     TDfunction (fn, e)
 
   | PDstruct { ps_name = { id; loc } } ->
-    let structure = Hashtbl.find contexte_structures id in
+    let structure = Option.get (Contexte.find id structures) in
 
     ( let rec get_recursive_struct_field { s_fields } acc =
         Seq.fold_left (find_recursive_struct_field acc) None (Hashtbl.to_seq_values s_fields)
@@ -322,14 +300,14 @@ let decl = function
 
 
 let file (imp, dl) =
-  List.iter phase1 dl;
-  List.iter phase2 dl;
+  let structures = List.fold_left phase1 Contexte.create dl in
+  let structures, functions = List.fold_left phase2 (structures, Contexte.create) dl in
 
   if not !found_main then error dummy_loc "missing method main";
 
-  let dl = List.map decl dl in
+  let dl = List.map (decl structures functions) dl in
 
-  Env.check_unused (); (* TODO variables non utilisees *)
+  (* Env.check_unused (); TODO variables non utilisees *)
 
   if imp && not !fmt_used then error dummy_loc "fmt imported but not used";
 
