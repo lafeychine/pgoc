4b93695459fb8d3e8dc76cbaa6b3538d6d2a1230
Vincent Lafeychine <vincent.lafeychine@gmail.com>
2021-12-04 13:44:40 +0100

    Test: Verify parameters

--- tests/bad/call/mismatch_param_type.go
+++ tests/bad/call/mismatch_param_type.go
@@ -1,6 +1,8 @@
 package main
 
-func foo(a int) {}
+func foo(a int) int {
+	return a
+}
 
 func main() {
 	foo("Hello")
@@ -8,6 +10,6 @@ func main() {
 
 /*
 == Expected compiler output ==
-File "./tests/bad/call/mismatch_param_type.go", line 6, characters 1-4:
+File "./tests/bad/call/mismatch_param_type.go", line 8, characters 1-4:
 error: cannot use type string as type int in argument to foo
 */
--- tests/bad/call/mismatch_params_number.go
+++ tests/bad/call/mismatch_params_number.go
@@ -1,6 +1,8 @@
 package main
 
-func foo(a int) {}
+func foo(a int) int {
+	return a
+}
 
 func main() {
 	foo(1, 2)
@@ -8,6 +10,6 @@ func main() {
 
 /*
 == Expected compiler output ==
-File "./tests/bad/call/mismatch_params_number.go", line 6, characters 1-4:
+File "./tests/bad/call/mismatch_params_number.go", line 8, characters 1-4:
 error: incorrect number of arguments in call to foo: 2 arguments but 1 parameters
 */
