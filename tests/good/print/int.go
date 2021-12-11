1e7a1da25508ae1218efb52e3ecce49cd50d3e1c
Vincent Lafeychine <vincent.lafeychine@gmail.com>
2021-12-11 17:27:04 +0100

    Tests: Early print

--- tests/good/print/int.go
+++ tests/good/print/int.go
@@ -7,13 +7,6 @@ func main() {
 }
 
 /*
-== Expected TAST ==
-TDfunction_1:fn_name = "main"
-root -> TDfunction_1
-TDfunction_1:fn_typ -> Tvoid_2
-TDfunction_1:expr -> TEblock_3
-TEblock_3:expr_list -> TEprint_4
-TEprint_4:expr_list -> TEconstant_5
-Cint_6:int64 = "12"
-TEconstant_5:constant -> Cint_6
+== Expected program output ==
+12
 */
--- tests/good/print/string.go
+++ tests/good/print/string.go
@@ -3,17 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("a")
+	fmt.Print("a\n")
 }
 
 /*
-== Expected TAST ==
-TDfunction_1:fn_name = "main"
-root -> TDfunction_1
-TDfunction_1:fn_typ -> Tvoid_2
-TDfunction_1:expr -> TEblock_3
-TEblock_3:expr_list -> TEprint_4
-TEprint_4:expr_list -> TEconstant_5
-Cstring_6:string = "a"
-TEconstant_5:constant -> Cstring_6
+== Expected program output ==
+a
 */
