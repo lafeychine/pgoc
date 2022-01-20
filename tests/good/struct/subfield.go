288e0835af60e499acae5724356391ae82a23123
Vincent Lafeychine <vincent.lafeychine@gmail.com>
2022-01-20 22:10:41 +0100

    Test: Hardened struct test

--- tests/good/struct/subfield.go
+++ tests/good/struct/subfield.go
@@ -3,26 +3,40 @@ package main
 import "fmt"
 
 type L struct {
-	x int
-	y M
+	lx int
+	ly M
 }
 
 type M struct {
-	x int
-	y string
+	mx int
+	my N
+	mz string
+}
+
+type N struct {
+	nx int
+	ny string
 }
 
 func main() {
 	var l L
 
-	l.x = 2
-	l.y.x = 3
-	l.y.y = "Hello"
+	l.lx = 2
+	l.ly.mx = 4
+	l.ly.my.nx = 5
+	l.ly.my.ny = "Hello"
+	l.ly.mz = "World"
 
-	fmt.Print(l.x, l.y.x, l.y.y, "\n")
+	fmt.Print(l.lx, l.ly.mx, l.ly.my.nx, l.ly.my.ny, l.ly.mz, "\n")
+	fmt.Print(l.lx, l.ly.mx, l.ly.my, l.ly.mz, "\n")
+	fmt.Print(l.lx, l.ly, "\n");
+	fmt.Print(l, "\n")
 }
 
 /*
 == Expected program output ==
-2 3Hello
+2 4 5HelloWorld
+2 4 {5 Hello}World
+2 {4 {5 Hello} World}
+{2 {4 {5 Hello} World}}
 */
