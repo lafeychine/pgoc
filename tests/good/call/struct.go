7433096d92e4163e9f1067942de4735770dac714
Vincent Lafeychine <vincent.lafeychine@gmail.com>
2022-01-23 17:33:52 +0100

    Tests: Add struct parameter

---
+++ tests/good/call/struct.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type L struct {
+	x int
+	y int
+}
+
+func foo(l L) {
+	fmt.Print(l)
+}
+
+func main() {
+	var l L
+
+	l.x = 42
+	l.y = 84
+
+	foo(l)
+}
+
+/*
+== Expected program output ==
+{42 84}
+*/
